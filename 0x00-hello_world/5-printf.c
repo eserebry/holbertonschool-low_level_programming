32344b3306afb51092ba58879711dacb14af48e3
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-18 03:20:05 +0000

    first attempt

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main  - print a string using function printf
+ * Return: Always 0 (Success)
+*/
+int  main(void)
+{
+printf("swith proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
