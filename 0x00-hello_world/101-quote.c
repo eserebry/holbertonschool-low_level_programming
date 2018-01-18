36cdcb76e8a6f8ff76b4ec83f2d04b29ffafe5e4
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-18 05:12:04 +0000

    first attempt

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main  - prints given error message, followed by a new line,
+ * to the standard error.
+ * Return: 1 (error)
+*/
+int  main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
