e5ec437d27716a03c51aa081473a0e3f9f0f9a22
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-19 01:52:47 +0000

    first attempt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main  - assign a random number to the variable n each time it is executed.
+ * Print the last digit of the number stored in the variable n.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
