b70789a64ae484db8870755984182be3b782e349
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-19 02:23:31 +0000

    first attempt

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main  - prints the alphabet in lowercase, followed by a new line.
+ * Print the last digit of the number stored in the variable n.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("123456789\n");
+	return (0);
+}
