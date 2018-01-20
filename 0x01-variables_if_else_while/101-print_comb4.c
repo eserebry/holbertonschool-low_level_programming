8dff652aadc2cb44dc1c7545dfb4c57f0ec1e84c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-20 06:18:00 +0000

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-#include <stdio.h>
-
 /**
- * main - prints all possible different combinations of two digits.
+ * main - prints all possible different combinations of three digits.
  * Return: Always 0 (Success)
  */
 int main(void)
