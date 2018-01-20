028028681dd95befeef417499bec10e85820080e
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-20 08:12:51 +0000

    third atttempt

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,8 +19,8 @@ int main(void)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
-					if  (((a == c) && (b < d))
-					     || ((a < c) && (b <= d)))
+					if  ((a == c && b < d)
+					     || (a < c && b <= d))
 					{
 						putchar(a);
 						putchar(b);
