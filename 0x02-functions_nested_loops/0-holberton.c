7daa15f960ec3f92235a43ec1fe605e03a1f7512
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-23 02:57:57 +0000

    firt commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * holberton  - prints Holberton, followed by a new line.
+ *
+ * Return: O, always success..
+ */
+int holberton(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
