d92571d33a9930f50a246a180b11ade70d3519fe
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-24 04:05:22 +0000

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @x: integer taken as an input
+ * @y: integet taken as an input
+ *
+ * Return: sum of x nad y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
