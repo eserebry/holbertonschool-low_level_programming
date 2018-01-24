1213a678a5bbe195c49d187b956193b2b68b0698
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-24 05:26:36 +0000

    first commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
  */
 int print_last_digit(int i)
 {
-	if (i < 0)
-		i = (-i) % 10;
-	else
-		i = i % 10;
-	_putchar(i + '0');
-	return (i);
+        if (i < 0)
+                i = (-i) % 10;
+        else
+                i = i % 10;
+        _putchar(i + '0');
+        return (i);
 }
