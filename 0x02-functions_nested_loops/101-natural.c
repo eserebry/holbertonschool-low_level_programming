2e84662a191c256ec8decb73eed9ae489c01deff
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-25 03:14:43 +0000

    first commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,19 +10,14 @@
 int main(void)
 {
 	int k;
-	int count;
 	int sum;
 
 	k = 1;
-	count = 0;
 	sum = 0;
 	while (sum < 1024)
 	{
 		if (k % 3 == 0 || k % 5 == 0)
-		{
-			count++;
 			sum += k;
-		}
 		k++;
 	}
 	printf("%d\n", sum);
