008bf097d2fe8414aa348a1a0f37899b7b685683
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-25 00:29:39 +0000

    first commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line.
+ * @n: integer taken as an input
+ *
+ * Return: sum of x nad y
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d ,", n);
+		n--;
+	}
+	if (n == 98)
+		printf("%d", n);
+	printf("\n");
+}
