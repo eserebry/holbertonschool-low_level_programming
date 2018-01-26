333ab1ca134160d26998cd24d133dff019139ac1
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-26 01:17:07 +0000

    FIRST COMMIT

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line, except 2 and 4.
+ *
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
