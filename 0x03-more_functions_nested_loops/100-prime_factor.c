af7e779a01792fb988e732aa0e4220a920714374
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-26 07:47:08 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include "stdio.h"
+
+/**
+ * main - finds and prints the largest prime factor
+ * of the number 612852475143, followed by a new line.
+ *
+ * Return: 0, always success.
+ */
+int  main(void)
+{
+	long int i;
+	long int k;
+	long int max;
+
+	k = 2;
+	i = 612852475143;
+	while (i > 1)
+	{
+		if (i % k == 0)
+		{
+			max = k;
+			i = i / k;
+		}
+		else
+			k++;
+	}
+		printf("%ld\n ", max);
+	return (0);
+}
