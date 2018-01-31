e37915e8480fdd8150289e64f998662f3975b5f5
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-31 05:03:30 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: pointer to a first element of a string s
+ */
+void rev_string(char *s)
+{
+	int i;
+	int k;
+	char t;
+
+	i = 0;
+	k = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (i != k)
+	{
+		t = s[k];
+		s[k] = s[i];
+		s[i] = t;
+		i--;
+		k++;
+	}
+}
