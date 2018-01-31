f7ece72ec699eacec28a58c11986f91523a89a39
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-31 04:12:25 +0000

    first commit

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,9 @@ void print_array(int *a, int n)
 	while (i <  n)
 	{
 		if (i == n - 1)
-			printf("%d", a[i]);
+			printf("%d\n", a[i]);
 		else
 			printf("%d, ", a[i]);
 		i++;
 	}
-	printf("\n");
 }
