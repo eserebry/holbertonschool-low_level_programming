e3209f3b91f0ce8a48d0f08cdee4a35bc271d572
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-31 04:08:48 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line.
+ *
+ * @a: pointer to the first element of array of integers.
+ * @n: number of elements of the array to be printed.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	while (i <  n)
+	{
+		if (i == n - 1)
+			printf("%d", a[i]);
+		else
+			printf("%d ,", a[i]);
+		i++;
+	}
+	printf("\n");
+}
