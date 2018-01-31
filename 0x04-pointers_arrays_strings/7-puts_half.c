0530a92b2b1ba505f55f6492db17fc90c78cd53a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-01-31 05:43:18 +0000

    second commit

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen - returns the lenght of the string.
- *
- * @s: pointer to a first element  of the string s
- * Return: i, lenght of the string.
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
  * puts_half - prints half of a string, followed by a new line.
  *
  * @str: pointer to the first element of the string str
@@ -26,18 +8,16 @@ int _strlen(char *s)
 void puts_half(char *str)
 {
 	int i;
-	int lenght;
 
 	i = 0;
-	lenght = _strlen(str);
-	if (lenght % 2 != 0)
+	while (str[i] != '\0')
 	{
-		i = (lenght - 1) / 2;
+		i++;
 	}
+	if (i % 2 != 0)
+		i = (i - 1) / 2;
 	else
-	{
-		i = lenght / 2;
-	}
+		i = i / 2;
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
