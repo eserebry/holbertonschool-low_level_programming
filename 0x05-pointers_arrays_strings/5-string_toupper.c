4806de3bc032305e556e84d55b2f778016348841
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-02 02:22:45 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @str: pointer to a first element of the string str
+ *
+ * Return: pointer to a resulating string str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
