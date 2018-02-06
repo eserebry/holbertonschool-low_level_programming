56710cedd72de64a379d71618f26801ff53492d8
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-06 06:14:08 +0000

    it would be magic if it works

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @haystack: ponter to a first element of the string haystack
+ * @needle: pointer to a first element of the string needle
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				return (needle);
+		}
+	}
+	return (0);
+}
