8f0fdd27274cccb476ad423cba5e0e4b393b3489
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-07 01:30:04 +0000

    fourth commit

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -16,13 +16,13 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for(j = 0; needle[j] >= '\0'; j++)
+		for (j = 0; needle[j] >= '\0'; j++)
 		{
 			if (haystack[i + j] != needle[j])
 				break;
-			if (needle[j] == '\0')
-				return(&haystack[i]);
 		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
 	}
 	return (0);
 }
