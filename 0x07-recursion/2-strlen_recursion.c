a26ffa5e9ae392688c826feb843b58dac629fb63
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-07 22:57:34 +0000

    second commit

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -9,15 +9,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	static int i;
 
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
-		return (i);
+		return (0);
 	}
 	else
 	{
-		i++;
-		return (_strlen_recursion(&s[i++]));
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
