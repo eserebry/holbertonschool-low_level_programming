7293f8a6e4485cb25663a4c20af2fc806e085809
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-08 07:37:19 +0000

    first commit

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ *
+ * @s1: pointer to a first element of the string s1
+ * @s2: pointer to a first element of the string s2
+ *
+ * Return: 1 if strings are equal, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (0);
+	if (*s1 == '*' && *s2 == '\0')
+		return (0);
+	if  (*s2 == '*')
+	{
+		if (s1 != '\0')
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	if (*s1 != *s2)
+		return (0);
+	else
+		return (0);
+}
