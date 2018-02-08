d1893ecdc89794bef07018bab9bb52aa73531fc5
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-08 06:32:11 +0000

    second commit

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _lenght - returns the length of a string.
  *
- * Return: Always 0.
+ * @s: pointer to a first element of the string s.
+ *
+ * Return: lenght of the  string
  */
 int _lenght(char *s)
 {
@@ -12,6 +14,17 @@ int _lenght(char *s)
 	else
 		return (1 + _lenght(s + 1));
 }
+
+/**
+ * iter - checking if the first and last characters of the string s are the
+ * same and so on and so on.
+ *
+ * @s: pointer to a first element of the string s, given as a parameter
+ * @i: integer i, responsible for a first half of the string
+ * @j: integer j, responsible for the second half of the string
+ *
+ * Return: 1, if string is a palindrome, 0 if its not, else calling herself
+ */
 int iter(char *s, int i, int j)
 {
 	if (i < j)
@@ -27,6 +40,13 @@ int iter(char *s, int i, int j)
 		return (0);
 }
 
+/**
+ * is_palindrome - checking if string is palindrome or not
+ *
+ * @s: pointer to a first element of the string s, given as a parameter
+ *
+ * Return: 1, if string is empty, else calling to the helper function iter
+ */
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
