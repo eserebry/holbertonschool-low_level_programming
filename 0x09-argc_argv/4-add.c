06ae060cdfba628c9f71f5bb56318cd154ffe212
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-12 04:11:12 +0000

    add a helper function

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * check - check if the elemant of the string integer ot not
+ *
+ * @argv: pointer, to a first element, of the string argv
+ *
+ * Return: 1 if the element of the string is int, 0 otherwise
+ */
+
+int check(char *argv)
+{
+	int i = 0;
+
+	while (argv[i] != '\0')
+	{
+		if (!(argv[i] >= '0' && argv[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - adds positive numbers.
  *
  * @argc: number of the argusments, passed to the program.
@@ -16,7 +37,7 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (*argv[i] >= '1' && *argv[i] <= '9')
+		if ((check(argv[i])))
 		{
 			sum += atoi(argv[i]);
 			i++;
