c911b4a50c293c59b7883f25231cfac3479c0d44
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-12 04:40:34 +0000

    added helper function

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * check - check if the elemant of the string integer ot not
+ *
+ * @argv: pointer, to a first element, of the string argv
+ *
+ * Return: 1 if the element of the string is int, 0 otherwise
+ */
+
+int check(char *argv)
+{
+	int i = 0;
+
+	while (argv[i] != '\0')
+	{
+		if (argv[i] == '-')
+			return (1);
+		if (!(argv[i] >= '0' && argv[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/**
  * main - prints the minimum number of coins to make change for an amount
  * of money.
  *
@@ -13,9 +35,11 @@
 
 int main(int argc, char *argv[])
 {
-	int i = atoi(argv[1]);
+	int i = 1;
 	int count = 0;
 
+	if (check(argv[i]))
+		i = atoi(argv[i]);
 	if (argc != 2 || argc == 0)
 	{
 		printf("Error\n");
@@ -23,30 +47,16 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		if (i < 0)
-			printf("0\n");
-		for (i = atoi(argv[1]); i >= 25; i -= 25)
+		for (; i >= 25; i -= 25)
 			count++;
-		while (i < 25 && i >= 10)
-		{
-			i -= 10;
+		for (; i < 25 && i >= 10; i -= 10)
 			count++;
-		}
-		while (i < 10  && i >= 5)
-		{
-			i -= 5;
+		for (; i < 10  && i >= 5; i -= 5)
 			count++;
-		}
-		while (i < 5 && i >= 2)
-		{
-			i -= 2;
+		for (; i < 5 && i >= 2; i -= 2)
 			count++;
-		}
-		while (i < 2 && i > 0)
-		{
-			i -= 1;
+		for (; i < 2 && i > 0; i -= 1)
 			count++;
-		}
 		printf("%d\n", count);
 	}
 	return (0);
