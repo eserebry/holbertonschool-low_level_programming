2239d512be42134d0872abf59db9093acb191375
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-13 23:05:19 +0000

    second commit

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -16,13 +16,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ar = (char*)malloc(size * sizeof(char));
 
+	if (size == 0)
+                return (NULL);
 	for  (i = 0; i < size; i++)
 	{
 		ar[i] = c;
 	}
-	if (size == 0)
-		return (NULL);
-	else
-		return (ar);
-	free (ar);
+	return (ar);
+	free(ar);
 }
