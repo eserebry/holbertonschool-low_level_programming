79cb61273db81edab95b7d2624f969b50e6dea02
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-14 03:24:33 +0000

    changing an empty string

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -35,9 +35,9 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	s3 = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
