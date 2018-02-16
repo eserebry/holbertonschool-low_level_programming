81eedd4248d1d0f1b25d1f7b27248c9debd4c16d
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-16 06:34:19 +0000

    allocating more space

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
-	s3 = malloc((_strlen(s1) + _strlen(s2)) * sizeof(char) + 1);
+	s3 = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
 	if (s3 == 0)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
