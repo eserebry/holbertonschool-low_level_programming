e684b29c1285397da2a009a6eac518f3364e6671
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-16 06:30:27 +0000

    changed edge cases

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
                 return (NULL);
-	ar = malloc((max - min) * sizeof(int) + 1);
+	ar = malloc((max - min + 1) * sizeof(int));
 	if (ar == 0)
 		return (NULL);
 	for (i = 0; min <= max; min++)
