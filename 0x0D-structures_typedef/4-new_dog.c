9bdad518e5fe2b13b9c86bb1566cd48ebf0aff8c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-20 05:30:56 +0000

    added a helper functions as well as allocating a space for a copy of name and owner

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,40 @@
 #include "stdlib.h"
 
 /**
+ * _strlen - returns lenght of the string
+ *
+ * @str: poiter to a first element of the str
+ *
+ * Return: lenght of the str
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strcpy - copys s1 into s2 and adds null terminating byte
+ *
+ * @s1: pointer to a first element of s1
+ * @s2: pointer to a first elelemt of s2
+ *
+ * Return: pointer to a first element of the s2
+ */
+char *_strcpy(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s2[i] = s1[i];
+	s2[i] = '\0';
+	return (s2);
+}
+
+/**
  * new_dog - creates a new dog.
  *
  * @name: name
@@ -12,13 +46,26 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *Dog;
+	char *name_cpy;
+	char *owner_cpy;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	Dog = malloc(sizeof(dog_t));
+	if (Dog == NULL)
 		return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-	return (dog);
+	name_cpy = malloc(sizeof(char) * _strlen(name));
+	if (name_cpy == NULL)
+		return (NULL);
+	owner_cpy = malloc(sizeof(char) * _strlen(owner));
+	if (owner_cpy == NULL)
+	{
+		free(name_cpy);
+		return (NULL);
+	}
+	_strcpy(name, name_cpy);
+	_strcpy(owner, owner_cpy);
+	Dog->name = name;
+	Dog->age = age;
+	Dog->owner = owner;
+	return (Dog);
 }
