e30be1f0ec5e70b9ec8242e26a5a5c8c96f38ac7
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-20 06:29:11 +0000

    first commit

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory
+ *
+ * @d: pointer to a structure
+ *
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
