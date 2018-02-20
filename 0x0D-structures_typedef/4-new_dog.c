38690c2f3c9182c1365ad54c1001f2cbc84a819c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-20 04:57:01 +0000

    first attempt

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+ * new_dog - creates a new dog.
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: pointer to a new structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
