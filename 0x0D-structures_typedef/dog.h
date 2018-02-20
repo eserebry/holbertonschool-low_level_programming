d00c3d5c55577ba7197cf377a36917f04a36f6b4
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-20 02:53:54 +0000

    first commit

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: I am descibing a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
