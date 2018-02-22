98d38f167f4ad482471e9a34f06de2a516b227fe
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-22 09:14:36 +0000

    library added

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCTIONPOINTERS_H_
 #define _FUNCTIONPOINTERS_H_
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
