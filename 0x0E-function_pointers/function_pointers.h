77fb045c1a0eaf6c6e5019440bb0d67cdd0068ce
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-22 01:11:51 +0000

    first commit

---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _FUNCTIONPOINTERS_H_
+#define _FUNCTIONPOINTERS_H_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /*_FUNCTIONPOINTERS_H_*/
