2c43a9b0592f958d5e8ada806c5be699c5e0aabb
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-24 03:59:43 +0000

    first commit

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ *
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *str;
+
+	if (separator == NULL)
+		return;
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(valist, char *);
+		if (str == NULL)
+			str = "nil";
+		if (i == (n - 1))
+			printf("%s", str);
+		else
+			printf("%s, ", str);
+	}
+	printf("\n");
+	va_end(valist);
+}
