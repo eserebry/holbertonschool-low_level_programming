5e04fa060b73e607a0f7952bcaaa414a79845b51
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-25 00:54:41 +0000

    typo coreected

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i == (n - 1))
 			printf("%d", va_arg(valist, unsigned int));
 		else
-			printf("%d ,", va_arg(valist, unsigned int));
+			printf("%d, ", va_arg(valist, unsigned int));
 	}
 	printf("\n");
 	va_end(valist);
