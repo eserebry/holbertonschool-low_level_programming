a2dc75a8fd67b118b2413567f67312ac534267d9
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-02-25 01:02:05 +0000

    library added

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,14 @@
 #ifndef _VARIADIC_H_
 #define _VARIADIC_H_
 
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct print{
+	char *print;
+	void (*f)(va_list);
+} print_t;
+	
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
