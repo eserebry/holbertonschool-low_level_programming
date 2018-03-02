ef5fbefbbfb9c5793e86817b728e363afbebd9a9
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-02 07:10:57 +0000

    first commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * print - prints before the main function is executed.
+ *
+ */
+void print (void) __attribute__ ((constructor));
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my houseupon my back!\n");
+}
