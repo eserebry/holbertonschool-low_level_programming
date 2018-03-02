b4c2491f55878419d59c59ab41629f7dc06adfea
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-02 07:16:43 +0000

    library added

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -4,7 +4,6 @@
  * print - prints before the main function is executed.
  *
  */
-void print (void) __attribute__ ((constructor));
 void print(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
