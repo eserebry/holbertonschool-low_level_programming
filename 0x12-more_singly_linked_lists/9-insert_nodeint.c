157b7bd9f439ef35d13466fed2f5d1fedbdcdb43
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 00:23:25 +0000

    edge case added

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 1;
 
 	if (head == NULL)
-		return (NULL);
+		return;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
