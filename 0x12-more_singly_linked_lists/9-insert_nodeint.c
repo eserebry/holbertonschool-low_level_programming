99beb5f6a0caae5800530660bc04c1d03668be05
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 06:42:51 +0000

    checking for case when head is == NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *tmp;
 	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
@@ -25,15 +23,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == (idx - 1))
 		{
-			if (tmp == NULL)
-				new->next = NULL;
-			else
-				new->next = tmp->next;
+			new->next = tmp->next;
 			tmp->next = new;
 			return (new);
 		}
 		tmp = tmp->next;
 		i++;
+	}
+	if (tmp == NULL)
+	{
+		new->next = NULL;
+		tmp->next = new;
+		return (new);
 	}
 	return (NULL);
 }
