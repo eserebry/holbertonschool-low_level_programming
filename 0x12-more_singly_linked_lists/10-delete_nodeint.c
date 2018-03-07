317556640a3e8188b06a6fd054e657d1e4bafe10
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 06:32:56 +0000

    return value changed

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,14 +13,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *previous;
 	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (-1);
 	tmp = *head;
 	if (tmp != NULL && index == 0)
 	{
 		*head = tmp->next;
 		free(tmp);
-		return (-1);
+		return (1);
 	}
 	while (tmp != NULL)
 	{
