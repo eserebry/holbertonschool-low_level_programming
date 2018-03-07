8d7085fd6af2bcaa9ff1913ff1b0509b861c4852
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 00:07:55 +0000

    edge case added

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx)
 		{
 			if (tmp == NULL)
-				return (NULL);
+				new->next = NULL;
 			else if (tmp != NULL)
 			{
 				new->next = tmp->next;
