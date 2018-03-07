c9411a14d249185b53855b55a32296890997c4dd
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 07:08:53 +0000

    edg case changed and betty checked '

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,8 +14,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *tmp;
 	unsigned int i = 0;
 
-        if (*head == NULL)
-                return (NULL);
+	if (head == NULL)
+		return (NULL);
 	tmp = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
