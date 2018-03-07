5e02f6432ad9fae561be33d7dbbd03d3b083a1a8
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 01:16:35 +0000

    checking the case for the index == 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *tmp;
-	unsigned int i = 1;
+	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
@@ -23,16 +23,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	while (tmp != NULL)
 	{
-		if (i == idx)
+		if (i == (idx - 1))
 		{
 			if (tmp == NULL)
 				new->next = NULL;
 			else if (tmp != NULL)
-			{
 				new->next = tmp->next;
-				tmp->next = new;
-				return (new);
-			}
+			tmp->next = new;
+			return (new);
 		}
 		tmp = tmp->next;
 		i++;
