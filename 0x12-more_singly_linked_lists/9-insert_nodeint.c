0be58a81fb06a09c0a62122aaf6353600db1c528
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 18:33:02 +0000

    changed the value ot the tmp

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,9 +21,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (tmp != NULL && idx == 0)
+	if (idx == 0)
 	{
-		new->next = tmp;
+		new->next = *head;
 		*head  = new;
 		return (new);
 	}
