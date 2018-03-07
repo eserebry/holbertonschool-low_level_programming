d592f9c7ecbc12430643a38bb0948672169da6c1
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-07 02:21:46 +0000

    first commit

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *previous;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (-1);
 	tmp = *head;
 	if (tmp != NULL && index == 0)
