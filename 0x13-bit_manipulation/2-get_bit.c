ceee6b1cd7d5c41642f286fd8d3a452ce517f7d6
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-09 07:13:07 +0000

    first commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_binary - returns the value of a bit at a given index.
+ *
+ * @n: integer to check
+ * @index: index to check
+ *
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int mask = 0;
+
+	if (index > (sizeof(unsigned int) * 8))
+		return (-1);
+	mask = 1;
+	mask = mask << index;
+
+	if ((mask & n) == 0)
+		return(0);
+	else
+		return(1);
+	return (-1);
+}
