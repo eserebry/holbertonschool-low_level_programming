bb3d0dfdf961b1427272631c20b46a1c51e77a64
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-09 07:41:36 +0000

    first commit

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ *
+ * @n: pointer to an integer n
+ * @index: index to check
+ *
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > (sizeof(unsigned int) * 8))
+		return (-1);
+
+	*n = ((0 << index) & *n);
+	return (1);
+}
