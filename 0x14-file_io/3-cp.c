931df525b91ef391e1bd1fad94ae248b6f1ad72b
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-14 17:58:10 +0000

    added helper functions

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,6 +1,41 @@
 #include "holberton.h"
 
 /**
+ * error_98 - exit with code 98 and print error msg
+ *
+ * @str: pointer to a string str
+ *
+ */
+void error_98(char *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+	exit(98);
+}
+
+/**
+ * error_99 - exit with code 99 and print error msg
+ *
+ * @str: pointer to a string str
+ *
+ */
+void error_99(char  *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+	exit(99);
+}
+
+/**
+ * error_100 - exit with code 100 and print error msg
+ *
+ * @str: pointer to a string str
+ *
+ */
+void error_100(char *str)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd  %s\n", str);
+	exit(100);
+}
+/**
  * main - copies the content of a file to another file.
  *
  * @ac: argument count
@@ -18,31 +53,25 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(av[1], O_RDWR);
+	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	file_to = open(av[2],  O_RDWR | O_TRUNC);
+		error_98(av[1]);
+	file_to = open(av[2],  O_RDWR | O_CREAT | O_TRUNC);
 	if (file_to == -1)
+		error_99(av[2]);
+	rd = 1024;
+	while (rd == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	rd = read(file_from, buff, 1024);
-	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		rd = read(file_from, buff, 1024);
+		if (rd == -1)
+			error_98(av[1]);
 	}
 	wr = write(file_to, buff, rd);
 	if (wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	close(file_from);
-	close(file_to);
+		error_99(av[2]);
+	if (close(file_from) == -1)
+		error_100(av[1]);
+	if (close(file_to) == -1)
+		error_100(av[2]);
 	return (0);
 }
