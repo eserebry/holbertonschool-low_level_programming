cecc3d91a45c895995f7c5ad202c5356842d2987
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-14 06:34:39 +0000

    first commit

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file.
+ *
+ * @ac: argument count
+ * @av: arguments, passed to a program
+ *
+ * Return: always 0
+ */
+int main(int ac, char *av[])
+{
+	int file_from, file_to, rd;
+	char buff[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(av[1], O_RDWR);
+	if (file_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	file_to = open(av[2],  O_RDWR | O_TRUNC);
+	if (file_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	rd = read(file_from, buff, 1024);
+	if (rd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	close(file_to);
+	close(file_from);
+	return (0);
+}
