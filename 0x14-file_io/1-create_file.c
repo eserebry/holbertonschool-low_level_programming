8b941a167658335835c6c6e02c20149d658df46c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-14 02:33:49 +0000

    typo edited

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -36,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (0);
 	}
-	write(fd, text_content, _strlen(text_content);
+	write(fd, text_content, _strlen(text_content));
 	close(fd);
 	return (1);
 }
