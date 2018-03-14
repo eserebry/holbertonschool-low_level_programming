16b7f458338163c44b4809b6d46781b5eccbd91c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-14 04:14:15 +0000

    first attempt

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the standard output.
+ *
+ * @filename: pointer to a name of the textfile
+ * @letters:  number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print, 0 otherwise
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *str;
+	size_t num;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+		return (0);
+	num = read(fd, str, letters);
+	write(1, str, num);
+	close(fd);
+	return (num);
+}
