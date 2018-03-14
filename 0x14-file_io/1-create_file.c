c768f2612ac8e87badf11f73a96d416987447b18
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-03-14 05:12:47 +0000

    file permission changed as well as O_TRUNC flag added

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -31,7 +31,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR, 600);
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (0);
