3d296612708a9125711ec1e1f31b8b238333f78c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-15 05:15:06 +0000

    edge case added

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp, *tmp1;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 	tmp = *head;
 	if (index == 0)
