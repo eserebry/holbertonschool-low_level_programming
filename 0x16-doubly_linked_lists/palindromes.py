de08b12b2c9c1ebd4bb9450156629ca768d4c0b5
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-16 04:58:21 +0000

    first commit

---
+++ 0x16-doubly_linked_lists/palindromes.py
@@ -0,0 +1,13 @@
+a, b = 999, 999
+for a in range(999, 100, -1):
+    for b in range(999, 100, -1):
+        mult = a * b
+        if (str(mult) == str(mult)[::-1]):
+            print("Palindrome is: {:d} * {:d} = {}" .format(a, b, str(mult)))
+            file = open("102-result", "w")
+            file.write(str(mult))
+            file.close()
+            exit()
+        else:
+            b -= 1
+        a -= 1
