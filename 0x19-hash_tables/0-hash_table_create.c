7713cc679f232ce671b1b79a6eff77df939e3d55
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-11 23:40:10 +0000

    defining size of created table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -14,9 +14,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	new_table = malloc(sizeof(hash_table_t) * size);
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
 	{
@@ -25,6 +26,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
-	new_table->size = size;
 	return (new_table);
 }
