23878cf68caf616ee3172090cf4d7657ee887bfa
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-11 23:32:51 +0000

    edge case added

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -12,8 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
 	new_table = malloc(sizeof(hash_table_t) * size);
-	if (new_table == NULL || size == 0)
+	if (new_table == NULL)
 		return (NULL);
 	new_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_table->array == NULL)
