48f8ad9b12ed71c1681e4252b798b0941cae8613
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-22 05:02:27 +0000

    mwmory allocation changed

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
-	new_table = malloc(sizeof(hash_node_t *));
+	new_table = malloc(sizeof(hash_node_t));
 	if (new_table == NULL)
 		return (0);
 	new_table->key = strdup(key);
