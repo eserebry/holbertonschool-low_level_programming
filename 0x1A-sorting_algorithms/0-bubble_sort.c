a5f6df94bce4277c3023d0a623a4097391b4fe40
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-30 07:09:30 +0000

    do not sort if size of the array is less than 2

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -12,16 +12,19 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int array_tmp = 0;
 
-	for (i = 0; i < size - 1; i++)
+	if (size >  2)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - i - 1; j++)
 			{
-				array_tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = array_tmp;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					array_tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = array_tmp;
+					print_array(array, size);
+				}
 			}
 		}
 	}
