33ff5a4210fb0b11c3a4f788a0e6f6d5db0d2868
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-31 06:31:07 +0000

    first commit

---
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,70 @@
+#include "sort.h"
+
+
+/**
+ * partition - Sorting helper function that partitions the array
+ * using the Lomuto partition scheme.
+ *
+ * @array: Array to be sorted.
+ * @lo: Low pointer.
+ * @hi: High pointer.
+ * @size: Length of array.
+ *
+ * Return: partition
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int pivot = array[hi], swap = 0;
+	int i = lo - 1, j;
+
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap = array[j];
+			array[j] = array[i];
+			array[i] = swap;
+			print_array(array, size);
+		}
+	}
+	swap = array[j];
+	array[j] = array[i + 1];
+	array[i + 1] = swap;
+	print_array(array, size);
+	return (i + 1);
+}
+
+
+/**
+ * q_s - Sorting helper function that will recursively call itself.
+ *
+ * @array: Array to be sorted.
+ * @lo: Low pointer.
+ * @hi: High pointer.
+ * @size: Length of array.
+ */
+void q_s(int *array, int lo, int hi, size_t size)
+{
+	int p;
+
+	if (lo < hi)
+	{
+		p = partition(array, lo, hi, size);
+		q_s(array, lo, p - 1, size);
+		q_s(array, p + 1, hi, size);
+	}
+}
+
+
+/**
+ * quick_sort - Sorts an array of integers
+ * in ascending order using the Quick sort algorithm.
+ *
+ * @array: Array to be sorted.
+ * @size: Length of array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	q_s(array, 0, size - 1, size);
+}
