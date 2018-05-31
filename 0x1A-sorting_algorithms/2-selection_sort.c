6fdebb92d79b7378b8c42258bd13acf6e3cb726a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-31 03:10:59 +0000

    do not print the result if notjing changed

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -23,12 +23,18 @@ void selection_sort(int *array, size_t size)
 			for (j = i + 1; j < size; j++)
 			{
 				if (array[j] < array[min])
+				{
 					min = j;
+				}
 			}
+			if (min != i)
+			{
 				tmp = array[i];
 				array[i] = array[min];
 				array[min] = tmp;
 				print_array(array, size);
+			}
+
 		}
 	}
 }
