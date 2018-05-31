54896591a7ce7e057b104456d8b6b9b361e21714
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-31 06:26:06 +0000

    edge cases added

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -11,23 +11,26 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *key = NULL;
 
-	key = (*list)->next;
-	while (key != NULL)
+	if (*list != NULL && list != NULL)
 	{
-		while (key->prev != NULL && key->prev->n > key->n)
+		key = (*list)->next;
+		while (key != NULL)
 		{
-			key->prev->next = key->next;
-			if (key->next != NULL)
-				key->next->prev = key->prev;
-			key->next = key->prev;
-			key->prev = key->prev->prev;
-			key->next->prev = key;
-			if (key->prev == NULL)
-				*list = key;
-			else
-				key->prev->next = key;
-			print_list(*list);
+			while (key->prev != NULL && key->prev->n > key->n)
+			{
+				key->prev->next = key->next;
+				if (key->next != NULL)
+					key->next->prev = key->prev;
+				key->next = key->prev;
+				key->prev = key->prev->prev;
+				key->next->prev = key;
+				if (key->prev == NULL)
+					*list = key;
+				else
+					key->prev->next = key;
+				print_list(*list);
+			}
+			key = key->next;
 		}
-		key = key->next;
 	}
 }
