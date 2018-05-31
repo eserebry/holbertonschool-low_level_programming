64bf94169cc5ec1a2ccc986a07f3633397668bbd
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-31 02:48:58 +0000

    first commit

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array : array to be sorted
+ * @size: size of the array to be sorted
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t min;
+	int tmp = 0;
+
+	if (size > 1)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			min = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < array[min])
+					min = j;
+			}
+				tmp = array[i];
+				array[i] = array[min];
+				array[min] = tmp;
+				print_array(array, size);
+		}
+	}
+}
