d2f63e1adfb58a2cb549a3a04da2b5dc4467ecde
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-06-16 07:01:29 +0000

    edge cases added

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -6,15 +6,16 @@ returns the perimeter of the island described in grid
 
 def island_perimeter(grid):
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
-                    perimeter += 1
-                if grid[i][j + 1] == 0:
-                    perimeter += 1
-                if grid[i + 1][j] == 0:
-                    perimeter += 1
-                if grid[i - 1][j] == 0:
-                    perimeter += 1
+    if grid != 0 and grid != [[]]:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    if grid[i][j - 1] == 0:
+                        perimeter += 1
+                    if grid[i][j + 1] == 0:
+                        perimeter += 1
+                    if grid[i + 1][j] == 0:
+                        perimeter += 1
+                    if grid[i - 1][j] == 0:
+                        perimeter += 1
     return perimeter
