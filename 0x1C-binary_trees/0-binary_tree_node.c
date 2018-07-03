9e4e2dd7b384a71a301a884d9715dd7cb79e09a4
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-03 00:31:42 +0000

    first commit

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,1 +1,25 @@
 #include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: pointer to a new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	if (parent == NULL)
+		node->parent = NULL;
+	else
+		node->parent = parent;
+	return (node);
+}
