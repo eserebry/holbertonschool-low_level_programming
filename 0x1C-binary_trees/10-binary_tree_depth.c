6fbda6d37e8775946c91fd6b1f09a080649b8297
maupinpamela <maupinpamela@gmail.com>
2018-07-03 23:44:53 +0000

    returns the depth of a binary tree

---
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - prints the depth of the binary tree
+ *
+ * @node: pointer to the root node of the tree to traverse
+ * Return: The depth of the tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	int counter = 0;
+
+	if (node == NULL)
+		return (0);
+
+	while (node->parent != NULL)
+	{
+		counter++;
+		node = node->parent;
+	}
+	return (counter);
+}
