d79924a4683c777808a2acd9a9243285ff39f767
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-05 18:21:33 +0000

    returning NULL if parent is NULL

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left = NULL;
 
-	new_left = binary_tree_node(parent, value);
 	if (parent == NULL)
-		return (NULL);
+                return (NULL);
+	new_left = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
 		new_left->left = parent->left;
