0cb63bac000eb46ea7fdd4b3e0f059dbf99fa517
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-05 18:25:08 +0000

    returnong NULL if parent is NULL

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_right = NULL;
 
-	new_right = binary_tree_node(parent, value);
 	if (parent == NULL)
 		return (NULL);
+	new_right = binary_tree_node(parent, value);
 	if (parent->right != NULL)
 	{
 		new_right->right = parent->right;
