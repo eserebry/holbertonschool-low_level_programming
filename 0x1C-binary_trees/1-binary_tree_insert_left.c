5f3c7782eb47b0ba6a6199eae45cc10bfc35f4fc
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-05 18:22:56 +0000

    betty fixed

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_left = NULL;
 
 	if (parent == NULL)
-                return (NULL);
+		return (NULL);
 	new_left = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
