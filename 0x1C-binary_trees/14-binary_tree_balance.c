9566924c0e008ad31852071255bee28df76548ac
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-06 04:44:13 +0000

    extra function returned

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,4 +1,26 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - prints the height of the binary tree
+ *
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: The height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftside, rightside;
+
+
+	if (tree == NULL)
+		return (0);
+
+	leftside = binary_tree_height(tree->left);
+	rightside = binary_tree_height(tree->right);
+	if (leftside < rightside)
+		return (rightside + 1);
+	else
+		return (leftside + 1);
+}
 
 /**
  * binary_tree_balance - returns the number of nodes that have a sibling
