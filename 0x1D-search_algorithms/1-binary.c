a1dff7b68d042466120ba9265e903c0838e36c5f
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-10 03:46:01 +0000

    first commit

---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size:  number of elements in array
+ * @value:  value to search for
+ *
+ * Return: first index where value is located, otherwise -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+	size_t med, low, high;
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		med = (low + high) / 2;
+		printf("Searching in array:");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i <  high)
+				printf(", ");
+			else
+				printf("\n");
+		}
+		if (value == array[med])
+			return (med);
+		if (value > array[med])
+		{
+			low  = med + 1;
+		}
+		else
+			high = med - 1;
+	}
+	return (-1);
+}
