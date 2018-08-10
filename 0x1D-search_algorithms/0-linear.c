4c4dcdfa6c43a3715cbad16d80de36c4793530e5
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-10 01:29:53 +0000

    edge case added

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -15,6 +15,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array [%zd] = [%d]\n", i, array[i]);
