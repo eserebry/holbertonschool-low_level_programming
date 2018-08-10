e2701af846a05239757d20db6e3d51e417e2a847
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-10 03:47:25 +0000

    white space added

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 	while (low <= high)
 	{
 		med = (low + high) / 2;
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
 			printf("%d", array[i]);
